3bd8ef3b19e878932e1d0d0881ad6a0f542e83ad
yoon-wish <sowon5685@gmail.com>
2024-04-01 16:14:35 +0900

    [Modify] init() 메소드 추가

--- BattleTime/src/BattleTime/StageTitle.java
+++ BattleTime/src/BattleTime/StageTitle.java
@@ -1,5 +1,27 @@
 package BattleTime;
 
-public class StageTitle extends Stage{
+public class StageTitle extends Stage {
 
+	@Override
+	public boolean update() {
+		System.out.println(".｡*ﾟ+.*.｡ BATTLE TIME ﾟ+..｡*ﾟ+");
+		System.out.println("[시작]을 입력하세요.");
+		System.out.print("👉 ");
+		String start = GameManager.sc.next();
+
+		while (!start.equals("시작")) {
+			System.out.print("👉 ");
+			start = GameManager.sc.next();
+		}
+
+		GameManager.nextStage = "LOBBY";
+
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
 }
