641a71c124a25b813778629c73d46f86df4c2802
yoon-wish <sowon5685@gmail.com>
2024-04-01 17:09:15 +0900

    [Modify] potion 변수 및 게터세터 제거

--- BattleTime/src/BattleTime/Player.java
+++ BattleTime/src/BattleTime/Player.java
@@ -5,15 +5,6 @@ public class Player extends Unit {
 	
 	Player(String name, int maxHp, int power){
 		super(name, maxHp, power);
-		this.potion = 0;
-	}
-	
-	public int getPotion() {
-		return this.potion;
-	}
-	
-	public void setPotion() {
-		this.potion ++;
 	}
 	
 	void skill() {
