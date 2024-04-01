e084ba097d0f7158f57d9c7302981256de691c4c
yoon-wish <sowon5685@gmail.com>
2024-04-01 17:10:21 +0900

    [ADD] UnitManager 클래스 작성

--- BattleTime/src/BattleTime/UnitManager.java
+++ BattleTime/src/BattleTime/UnitManager.java
@@ -1,5 +1,41 @@
 package BattleTime;
 
-public class UnitManager {
+import java.util.ArrayList;
 
+public class UnitManager {
+	private ArrayList<Unit> monster_list;
+	public static ArrayList<Player> player_list;
+	
+	private String path = "BattleTime.";
+	private String monNames[] = { "Bat", "Dracula", "Orc" };
+	
+	public UnitManager() {
+		monster_list = new ArrayList<>();
+		player_list = new ArrayList<>();
+		
+		player_list.add(new Player("전사", 1000, 45));
+		player_list.add(new Player("마법사", 800, 60));
+		player_list.add(new Player("힐러", 500, 70));
+	}
+	
+	public void monster_rand_set(int size) {
+		for(int i=0; i<size; i++) {
+			int num = GameManager.rand.nextInt(monNames.length);
+			try {
+				Class<?> clazz = Class.forName(path + monNames[num]);
+				Object obj = clazz.getDeclaredConstructor().newInstance();
+				
+				Unit temp = (Unit) obj;
+				int hp = GameManager.rand.nextInt(100) + 100;
+				int power = GameManager.rand.nextInt(10) + 10;
+				
+				temp.init(hp, power);
+				monster_list.add(temp);
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
