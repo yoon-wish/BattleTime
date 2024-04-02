811e4a317b2c4e863a7fe9a745f26177e913ef87
yoon-wish <sowon5685@gmail.com>
2024-04-02 11:09:52 +0900

    [ADD] 저장 기능 추가

--- BattleTime/src/BattleTime/StageLobby.java
+++ BattleTime/src/BattleTime/StageLobby.java
@@ -3,14 +3,16 @@ package BattleTime;
 public class StageLobby extends Stage {
 	private final int VILLAGE = 1;
 	private final int BATTLE = 2;
-	private final int EXIT = 3;
+	private final int SAVE = 3;
+	private final int EXIT = 4;
 
 	@Override
 	public boolean update() {
 		System.out.println("┌──────────────┐");
 		System.out.println("    ❶ 마을");
 		System.out.println("    ❷ 전투");
-		System.out.println("    ❸ 종료");
+		System.out.println("    ❸ 저장");
+		System.out.println("    ❹ 종료");
 		System.out.println("└──────────────┘");
 		System.out.print("👉 ");
 		int sel = GameManager.sc.nextInt();
@@ -23,19 +25,23 @@ public class StageLobby extends Stage {
 		if (sel == VILLAGE)
 			GameManager.nextStage = "VILLAGE";
 		else if (sel == BATTLE) {
-			if(GameManager.battleNum == 0) {
+			if (GameManager.battleNum == 0) {
 				System.out.println("┌────────────────────────────────────┐");
 				System.out.println("   오늘 이미 전투를 치뤘다");
 				System.out.println("   다들 지친 상태이다");
 				System.out.println("   휴식을 취하고 다음 날 다시 오자");
 				System.out.println("└────────────────────────────────────┘");
 				GameManager.nextStage = "LOBBY";
-			}else
+			} else
 				GameManager.nextStage = "BATTLE";
-		}
-		else if (sel == EXIT)
+		} else if (sel == SAVE) {
+			GameManager.fileManager.save(saveInfo());
+			GameManager.nextStage = "LOBBY";
+		} else if (sel == EXIT) {
+			GameManager.day += 1;
 			GameManager.fileManager.save(saveInfo());
 			GameManager.nextStage = "";
+		}
 
 		return false;
 	}
@@ -45,7 +51,7 @@ public class StageLobby extends Stage {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	private String saveInfo() {
 		// 날짜/포션/코인/몬스터Hp
 		return GameManager.day + "/" + GameManager.potion + "/" + GameManager.coin + "/" + GameManager.ranHp;
